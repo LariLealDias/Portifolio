3b4bed91c6469e02bd4d9aab5677dad27ec09a90
Lari <larileal@hotmail.com.br>
2023-06-22 12:23:05 -0300

    update Habilidades

--- src/app/componentes/habilidades/habilidades.component.ts
+++ src/app/componentes/habilidades/habilidades.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HabilidadesComponent implements OnInit {
 
+  isHovered = false;
+  text1 = "C#";
+  text2 = "NodeJs";
+  text3 = "MySql";
+  text4 = "Angular";
+  text5 = "TS";
+  text6 = "React";
+  text7 = "JS";
+  text8 = "HTML";
+  text9 = "CSS";
+  text10 = "VS";
+  text11 = "VSC";
+  text12 = "Postman";
+  text13 = "Git e GitHub";
+  text14 = "ASP.NET";
+
+
+
+
+
+
+  hoveredText = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    this.hoveredText = '';
   }
 
 }
